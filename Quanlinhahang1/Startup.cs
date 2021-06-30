0b4e57b4f70047bf9609e58aea0424a564cd68b8
197PM21993 - NGUYỄN ĐÌNH TRƯỜNG - K25T-PM1 <truong.197pm21993@vanlanguni.vn>
2021-06-30 14:42:00 +0700

    Add project files.

---
+++ Quanlinhahang1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quanlinhahang1.Startup))]
+namespace Quanlinhahang1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
